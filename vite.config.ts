ff73f130779489b810024da26f83e7f777b40dac
Alan North <alanscodelog@gmail.com>
2023-07-18 23:51:26 -0300

    fix: fixed deps not getting properly externalized
    - added git hook to avoid issue in the future

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { escapeRegex } from "@alanscodelog/utils"
 import vue from "@vitejs/plugin-vue"
 import glob from "fast-glob"
 import fs from "fs"
@@ -15,7 +16,18 @@ import postcss from "./postcss.config.js"
 const folders = fs.readdirSync("src", { withFileTypes: true })
 	.filter(file => file.isDirectory() && !["types", "types.ts", "bin"].includes(file.name))
 	.map(file => file.name)
+
 const fileEntries = glob.sync(path.resolve(__dirname, `src/{${folders.join((","))}}/*.{ts, vue}`))
+	.filter(filepath => !filepath.endsWith(".stories.ts"))
+
+// It is not enought to list the packages since we will not get matches when importing files inside the package. This converts the external dependencies to a regex to properly match all imports from a package.
+// We can be sure none escape by checking in the build output that there are no dist/node_modules files, this is done by a pre-push git hook.
+const external = [
+	...builtinModules,
+	...Object.keys((packageJson as any).dependencies ?? {}),
+	...Object.keys((packageJson as any).peerDependencies ?? {}),
+	"@babel/runtime",
+].map(pkg => new RegExp(`^${escapeRegex(pkg)}($|\\/)`))
 
 // return
 // https://vitejs.dev/config/
@@ -28,6 +40,7 @@ export default async ({ mode }: { mode: string }) => defineConfig({
 		}),
 		dts({
 			entryRoot: "src",
+			tsconfigPath: "tsconfig.types.json",
 		}),
 	],
 	build: {
@@ -44,7 +57,8 @@ export default async ({ mode }: { mode: string }) => defineConfig({
 			formats: ["es"],
 		},
 		rollupOptions: {
-			external: [...builtinModules, ...Object.keys((packageJson as any).dependencies ?? {}), ...Object.keys((packageJson as any).peerDependencies ?? {}), /@babel\/runtime/],
+			external,
+
 			output: {
 				preserveModules: true,
 				preserveModulesRoot: "src",
@@ -68,7 +82,6 @@ export default async ({ mode }: { mode: string }) => defineConfig({
 	},
 	optimizeDeps: {
 		include: [
-			// "@alanscodelog/utils",
 		],
 	},
 	server: {
