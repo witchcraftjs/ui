ceb7f13c0385c0e19a4def2023b05eec8f588bb4
Alan North <alanscodelog@gmail.com>
2023-07-22 10:50:08 -0300

    feat: useDarkMode composable

---
+++ src/composables/useDarkMode.ts
@@ -0,0 +1,39 @@
+import { computed, onMounted, ref, watch } from "vue"
+
+
+// future add lib name?
+const defaultLocalStorageKey = "prefersColorSchemeDark"
+export const useDarkMode = ({ useLocalStorage }: { useLocalStorage: boolean | string }) => {
+	const systemDarkMode = ref(false)
+	const manualDarkMode = ref<boolean | undefined>(undefined)
+	if (useLocalStorage) {
+		watch(manualDarkMode, () => {
+			localStorage.setItem(defaultLocalStorageKey, manualDarkMode.value ? "true" : "false")
+		})
+	}
+
+	const darkMode = computed(() => manualDarkMode.value ?? systemDarkMode.value)
+
+	onMounted(() => {
+		window.matchMedia("(prefers-color-scheme: dark)")
+			.addEventListener("change", ({ matches }) => {
+				if (matches) {
+					systemDarkMode.value = true
+				} else {
+					systemDarkMode.value = false
+				}
+			})
+		if (useLocalStorage !== false) {
+			const key = typeof useLocalStorage === "string" ? useLocalStorage : defaultLocalStorageKey
+			const value = localStorage.getItem(key)
+
+			if (value === "true") {
+				manualDarkMode.value = true
+			} else if (value === "false") {
+				manualDarkMode.value = false
+			}
+		}
+	})
+
+	return { darkMode, manualDarkMode, systemDarkMode }
+}
