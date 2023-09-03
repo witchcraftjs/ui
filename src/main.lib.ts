ef3dc1376b952b27036c5a36a5b91203b47419f8
Alan North <alanscodelog@gmail.com>
2023-09-03 18:27:56 -0300

    feat: added function to register only some components
    Treeshaking doesn't work if registering all components globally with the plugin.
    This allows more selective importing.

--- src/main.lib.ts
+++ src/main.lib.ts
@@ -12,17 +12,43 @@ import { capitalize } from "@alanscodelog/utils"
 
 import * as components from "./components/index.js"
 
+
+/**
+ * Register only specific components. Will register components as both snake case and pascal case.
+ *
+ * ```ts
+ * import {SomeComponent} from "@alanscodelog/vue-components"
+ * const components = { SomeComponent }
+ * createApp(App)
+ * 	.use({
+ * 		install(app: typeof App) {
+ * 			// will register optional-prefix-some-component and optionalPrefixSomeComponent
+ * 			// note it doesn't matter what you call the component in the Object
+ * 			// its name is set internally already
+ * 			registerComponents(app, components, "optional-prefix-")
+ * 		},
+ *	})
+ * ```
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-shadow
+export const registerComponents = (app: App, components: Record<string, any>, prefix = ""): void => {
+	for (const key of Object.keys(components)) {
+		const component = (components as any)[key]
+		const name = prefix + component.name
+		const capitalizedname = name.split("-").map((_: string) => capitalize(_)).join("")
+		app.component(name, component)
+		app.component(capitalizedname, component)
+	}
+}
+
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const VueComponentsPlugin: Plugin = {
 	install(app: App) {
-		for (const componentName of Object.keys(components)) {
-			const component = (components as any)[componentName]
-			const capitalizedName = component.name.split("-").map((_: string) => capitalize(_)).join("")
-			app.component(component.name, component)
-			app.component(capitalizedName, component)
-		}
+		registerComponents(app, components)
 	},
 }
+
 
 type Components = typeof components
 
