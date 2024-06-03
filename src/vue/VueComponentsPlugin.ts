2707d836156b987e8007ee28c64db1cdd0edad4e
Alan North <alanscodelog@gmail.com>
2024-06-03 18:01:43 -0300

    fix: fixed missing components import

--- src/vue/VueComponentsPlugin.ts
+++ src/vue/VueComponentsPlugin.ts
@@ -3,15 +3,13 @@ import type { App, Plugin } from "vue"
 import { registerComponents } from "./registerComponents.js"
 import { registerDirectives } from "./registerDirectives.js"
 
-export * as directives from "../directives/index.js"
-export * as components from "../components/index.js"
+import * as components from "../components/index.js"
+import * as directives from "../directives/index.js"
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const VueComponentsPlugin: Plugin = {
 	install(app: App) {
-		// @ts-expect-error .
 		registerComponents(app, components)
-		// @ts-expect-error .
 		registerDirectives(app, directives)
 	},
 }
