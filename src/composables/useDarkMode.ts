d750367c58afd5051dfb11f96e2541a6e4a79957
Alan North <alanscodelog@gmail.com>
2024-06-10 21:40:09 -0300

    feat: added several options an features to useDarkMode

--- src/composables/useDarkMode.ts
+++ src/composables/useDarkMode.ts
@@ -1,24 +1,62 @@
 import { computed, onMounted, type Ref,ref, watch } from "vue"
+export type DarkModeOptions = {
+	/* Whether to save the manual dark mode to local storage. Uses the key "prefersColorSchemeDark" by default. You can pass a key instead of true to use that as the key instead. */
+	useLocalStorage?: boolean | string
+	/* The order of the string dark modes when using `cycleDarkMode`. Defaults to `["system", "dark", "light"]` */
+	darkModeOrder?: readonly ("system" | "dark" | "light")[]
+}
 
-
-// future add lib name?
 const defaultLocalStorageKey = "prefersColorSchemeDark"
+const defaultOrder = ["system", "dark", "light"] as const
+
 export const useDarkMode = ({
 	useLocalStorage,
-}: { useLocalStorage?: boolean | string } = {}): {
-	darkMode: Ref<boolean>
-	manualDarkMode: Ref<boolean | undefined>
-	systemDarkMode: Ref<boolean>
-} => {
+	darkModeOrder = defaultOrder
+}: DarkModeOptions = {}): {
+		/** Whether the dark mode should be enabled or not */
+		darkMode: Ref<boolean>
+		/** The current state of the darkMode but as a string (dark, light, system) */
+		darkModeState: Ref<"dark" | "light" | "system">
+		/** The value of the manuably controllable dark mode. You can set this to true/false or undefined to allow the systemDarkMode to take priority. Alternatively use setDarkMode instead. */
+		manualDarkMode: Ref<boolean | undefined>
+		/** The value of the system dark mode. This is automatically set depending on the user's `prefer-color-scheme` and should not be set directly. */
+		systemDarkMode: Ref<boolean>
+		setDarkMode: (value: "dark" | "light" | "system") => void
+		cycleDarkMode: () => void
+	} => {
 	const systemDarkMode = ref(false)
 	const manualDarkMode = ref<boolean | undefined>(undefined)
-	if (useLocalStorage) {
-		watch(manualDarkMode, () => {
+	watch(() => useLocalStorage && manualDarkMode, () => {
+		if (useLocalStorage !== false) {
 			localStorage.setItem(defaultLocalStorageKey, manualDarkMode.value ? "true" : "false")
-		})
-	}
+		}
+	})
 
 	const darkMode = computed(() => manualDarkMode.value ?? systemDarkMode.value)
+	const darkModeState = computed(() =>
+		manualDarkMode.value === undefined
+		? "system"
+		: manualDarkMode.value
+		? "dark"
+		: "light"
+	)
+	function setDarkMode(value: "dark" | "light" | "system"): void {
+		manualDarkMode.value =
+			value === "dark"
+			? true
+			: value === "light"
+			? false
+			: undefined
+	}
+	function cycleDarkMode(): void {
+		const index = darkModeOrder.indexOf(darkModeState.value)
+
+		if (index === 2) {
+			setDarkMode(darkModeOrder[0])
+		} else {
+			setDarkMode(darkModeOrder[index + 1])
+		}
+	}
 
 	onMounted(() => {
 		window.matchMedia("(prefers-color-scheme: dark)")
@@ -41,5 +79,13 @@ export const useDarkMode = ({
 		}
 	})
 
-	return { darkMode, manualDarkMode, systemDarkMode }
+	return {
+		darkMode,
+		darkModeState,
+		setDarkMode,
+		cycleDarkMode,
+		manualDarkMode,
+		systemDarkMode,
+	}
 }
+export const defaultDarkModeOrder = defaultOrder
