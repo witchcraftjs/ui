e6ff086422ec735bca8e3cc31ed4ffed7df25fd7
Alan North <alanscodelog@gmail.com>
2024-06-26 18:40:29 -0300

    feat: added export of the twMerge config used (twMergeExtend)

--- src/helpers/twMerge.ts
+++ src/helpers/twMerge.ts
@@ -1,10 +1,11 @@
 import { extendTailwindMerge } from "tailwind-merge"
 
-
-export const twMerge = extendTailwindMerge<"focus-outline">({
+export const twMergeExtend = {
 	extend: {
 		classGroups: {
 			"focus-outline": [{ "focus-outline": ["", "no-offset", "none"]}],
 		},
 	}
-})
+} satisfies Parameters<typeof extendTailwindMerge>[0]
+
+export const twMerge = extendTailwindMerge<"focus-outline">(twMergeExtend)
