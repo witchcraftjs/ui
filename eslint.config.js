c0ffe52da8015450fb2bda55dc6c9cc1e8e9d482
Alan North <alanscodelog@gmail.com>
2024-10-10 19:08:48 -0300

    deps: updated tsconfig and vite config
    BREAKING CHANGE: Dropped support for vite built demo

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,44 @@
-import { allFileTypes, tsEslintConfig, vueConfig } from "@alanscodelog/eslint-config"
-export default tsEslintConfig(
-	// https://github.com/AlansCodeLog/eslint-config
-	...vueConfig,
-	{
-		files: [`**/*.{${allFileTypes.join(",")}}`],
-		languageOptions: {
-			parserOptions: {
-				// was causing issues, went back to old usage of separate eslint config
-				// eslint-disable-next-line camelcase
-				EXPERIMENTAL_useProjectService: false,
-				project: "./tsconfig.eslint.json",
-				tsconfigRootDir: import.meta.dirname,
+import { vueConfig } from "@alanscodelog/eslint-config"
+import { createConfigForNuxt } from "@nuxt/eslint-config/flat"
+
+// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
+export default createConfigForNuxt({
+	features: {
+		tooling: false, // is overriding standalone?
+		stylistic: false,
+		standalone: false
+	},
+	dirs: {
+		src: [
+			"./playground",
+		],
+	},
+})
+	.append(
+		...vueConfig,
+		{
+			files: ["**/*"],
+			ignores: [".nuxt/"],
+			// for auto imports
+			rules: {
+				"no-undef": "off",
 			},
 		},
-	},
-	{
-		files: [`**/*.stories.*`],
-		rules: {
-			"@typescript-eslint/explicit-function-return-type": "off"
-		}
-	},
-	{
-		files: [`**/*.vue`],
-		rules: {
-			"jsdoc/check-tag-names": ["warn", {
-				definedTags: [
-					"vue-ignore",
-				]
-			}]
-		}
-	}
-	// RULE LINKS
-	// Eslint: https://eslint.org/docs/rules/
-	// Typescript: https://typescript-eslint.io/rules/
-	// Vue: https://eslint.vuejs.org/rules/
-)
+		{
+			files: [`**/*.stories.*`],
+			rules: {
+				"@typescript-eslint/explicit-function-return-type": "off"
+			}
+		},
+		{
+			files: [`**/*.vue`],
+			rules: {
+				"jsdoc/check-tag-names": ["warn", {
+					definedTags: [
+						"vue-ignore",
+					]
+				}]
+			}
+		},
+		
+	)
--- vite.config.ts
+++ vite.config.ts
@@ -1,102 +1,45 @@
-import { run } from "@alanscodelog/utils/node"
+import { defineConfig } from "@alanscodelog/vite-config"
 import vue from "@vitejs/plugin-vue"
-import glob from "fast-glob"
-import fs from "fs"
-import path from "path"
 import IconsResolver from "unplugin-icons/resolver"
 import Icons from "unplugin-icons/vite"
 import Components from "unplugin-vue-components/vite"
-import { defineConfig,type PluginOption } from "vite"
-// import Previewer from 'vite-plugin-vue-component-preview';
-import { externalizeDeps } from "vite-plugin-externalize-deps"
 
-// @ts-expect-error .
+// workaround for esm/ts import issues
 import postcss from "./postcss.config.js"
 import { unpluginIconViteOptions } from "./src/build/unpluginIconViteOptions.js"
 
-
-const folders = fs.readdirSync("src", { withFileTypes: true })
-	.filter(file => file.isDirectory() && !["types", "types.ts", "bin"].includes(file.name))
-	.map(file => file.name)
-
-const fileEntries = glob.sync(path.resolve(__dirname, `src/{${folders.join((","))}}/*.{ts, vue}`))
-	.filter(filepath => !filepath.endsWith(".stories.ts"))
-
-const typesPlugin = (): PluginOption => ({
-	name: "typesPlugin",
-	// eslint-disable-next-line no-console
-	writeBundle: async () => run(`npm run build:types`).promise.catch(e => { console.log(e); process.exit(1) }).then(() => undefined),
-})
-
-
-// https://vitejs.dev/config/
-export default async ({ mode }: { mode: string }) => defineConfig({
+export default defineConfig({
+	entryGlobs: [
+		"src/**/*.ts",
+		"src/**/*.vue",
+		"!src/**/*.stories.*",
+		"!src/nuxt/**/*",
+		"!src/module.ts",
+	],
+	pluginOpts: {
+		// just for ./src/nuxt/*
+		externalizeDeps: { include: [
+			"#imports",
+		]},
+		typesPlugin: { dtsGenerator: "vue-tsc" }
+	},
+}, {
 	plugins: [
-		// it isn't enough to just pass the deps list to rollup.external since it will not exclude subpath exports
-		externalizeDeps(),
-		vue({
-			script: {
-				defineModel: true,
-			},
-		}),
-		// runs build:types script which takes care of generating types
-		// note aliases and baseUrl imports should not be used, they have all been removed
-		typesPlugin(),
+		vue() as any,
 		Components({
-			dirs: ["src/components/Icon"], // don't auto-import our own components except icon
-			resolvers: [
-				IconsResolver({
-				}),
-			],
+			// don't auto-import our own components
+			dirs: [],
+			resolvers: [ IconsResolver()],
+			dts: "./types/components.d.ts",
 		}),
 		Icons(unpluginIconViteOptions)
 	],
 	build: {
-		outDir: "dist",
-		lib: {
-			entry: [
-				"src/main.lib.ts",
-				"src/theme.ts",
-				"src/types.ts",
-				"src/globalResizeObserver.ts",
-				...fileEntries,
-			],
-			formats: ["es"],
-		},
-		rollupOptions: {
-			output: {
-				preserveModulesRoot: "src",
-				preserveModules: true,
-			},
-		},
-		minify: false,
-		...(mode === "production" ? {
-		} : {
-			sourcemap: "inline",
-		}),
+		emptyOutDir: false,
 	},
-	css: {
-		postcss,
-	},
-	resolve: {
-		alias: {
-			// aliases not currently being used because nuxt cannot handler them when importing directly from components :(
-		},
-	},
-	optimizeDeps: {
-		include: [
-		],
-	},
+},{
+	css: { postcss },
 	server: {
 		port: 3001,
-		fs: {
-			allow: [...(process.env.CODE_PROJECTS ?? [])!],
-		},
-		watch: {
-			// for pnpm
-			followSymlinks: true,
-			// watch changes in linked repos
-			ignored: ["!**/node_modules/@alanscodelog/**"],
-		},
 	},
 })
