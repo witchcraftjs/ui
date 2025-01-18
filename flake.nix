24d60a17d29b8570726bd7d7d14bd7934bb66bfc
Alan North <alanscodelog@gmail.com>
2025-01-18 18:33:59 -0300

    build: updated flake

--- flake.nix
+++ flake.nix
@@ -29,31 +29,18 @@
             default = devenv.lib.mkShell {
               inherit inputs pkgs;
               modules =
-                # https://discourse.nixos.org/t/running-playwright-tests/25655/33
-                let
-                  playwright-driver = pkgs.playwright-driver;
-                  playwright-driver-browsers = pkgs.playwright-driver.browsers;
-
-                  playright-file = builtins.readFile "${playwright-driver}/package/browsers.json";
-                  playright-json = builtins.fromJSON playright-file;
-                  playwright-chromium-entry = builtins.elemAt
-                    (builtins.filter
-                      (
-                        browser: browser.name == "chromium"
-                      )
-                      playright-json.browsers) 0;
-                  playwright-chromium-revision = playwright-chromium-entry.revision;
-                in
                 [
                   {
-env.PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
-                    env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH = "${playwright-driver-browsers}/chromium-${playwright-chromium-revision}/chrome-linux/chrome";
+
                     # This is used by npx playwright --{ui,debug,...}
-                    env.PLAYWRIGHT_BROWSERS_PATH = "${playwright-driver-browsers}";
+                    # https://discourse.nixos.org/t/running-playwright-tests/25655/33
+                    env.PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+                    env.PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
+                    env.PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
                     languages = {
                       javascript = {
                         enable = true;
-                      package = pkgs.nodejs_22;
+                      package = pkgs.nodejs_23;
                         pnpm.enable = true;
                       };
                     };
