91ceb822c3d6f39be6f924b1758a0283e0d8ee7c
Alan North <alanscodelog@gmail.com>
2025-04-01 20:55:09 -0300

    build: fixed files missing in dist

--- build.config.ts
+++ build.config.ts
@@ -1,12 +1,29 @@
+import { globSync as glob } from "fast-glob"
 import { defineBuildConfig } from "unbuild"
 
+const components = glob(["./src/runtime/components/*/**"], { onlyDirectories: true }).map(_ => _.slice("./src/".length))
 export default defineBuildConfig({
 	entries: [
+		"src/module.ts",
 		{
 			builder: "mkdist",
-			input: "./src/runtime/**/",
-			outDir: "./dist/runtime/**/",
+			input: "./src/runtime/",
+			outDir: "./dist/runtime/",
 			ext: "js",
 		},
+		{
+			builder: "mkdist",
+			input: "./src/runtime/components/",
+			outDir: "./dist/runtime/components/",
+			ext: "js",
+		},
+		...components.map(folder => ({
+			builder: "mkdist" as const,
+			input: `./src/${folder}`,
+			outDir: `./dist/${folder}`,
+			ext: "js",
+			format: "esm",
+		}))
+
 	]
 })
