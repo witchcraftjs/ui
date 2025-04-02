fecee34790e289825bd6099648570cb3e9a44574
Alan North <alanscodelog@gmail.com>
2025-04-02 14:15:40 -0300

    feat: added ability to override theme in Root component

--- src/runtime/components/LibRoot/LibRoot.vue
+++ src/runtime/components/LibRoot/LibRoot.vue
@@ -20,16 +20,16 @@
 			bg-bg
 			text-fg
 		`,
-		testWrapperMode && `
+			testWrapperMode && `
 			px-10
 			pb-10
 		`,
-		!testWrapperMode && `
+			!testWrapperMode && `
 			min-h-dvh
 			flex
 			flex-col
 		`,
-		($attrs as any).attrs?.class)"
+			($attrs as any).attrs?.class)"
 	>
 		<TestControls v-if="testWrapperMode" :show-outline="showOutline"/>
 		<slot/>
@@ -38,6 +38,7 @@
 </template>
 
 <script setup lang="ts">
+import { unreachable } from "@alanscodelog/utils/unreachable.js"
 import { type Theme } from "metamorphosis"
 import { type ComponentPublicInstance, computed, onBeforeUnmount, onMounted, ref, toRaw } from "vue"
 
@@ -45,26 +46,29 @@ import { useAccesibilityOutline } from "../../composables/useAccesibilityOutline
 import { useDivideAttrs } from "../../composables/useDivideAttrs.js"
 import { useSetupDarkMode } from "../../composables/useSetupDarkMode.js"
 import { useShowDevOnlyKey } from "../../composables/useShowDevOnlyKey.js"
-import { theme } from "../../theme.js"
+import { theme as defaultTheme } from "../../theme.js"
 import { twMerge } from "../../utils/twMerge.js"
+import {type Theme} from "metamorphosis"
 import TestControls from "../TestControls/TestControls.vue"
-import { unreachable } from "@alanscodelog/utils/unreachable.js"
 
 const $attrs = useDivideAttrs(["wrapper"])
 
 defineOptions({ name: "root", inheritAttrs: false })
 const props = withDefaults(defineProps<{
+	theme?: Theme
 	outline?: boolean
 	forceOutline?: boolean
 	testWrapperMode?: boolean
 	id?: string
-	/** You can set a ref to the root element by passing :ref="_ => el = _" */
-	ref?: (el: HTMLElement | null) => void
+	/** You can set a ref to the root element by passing :getRef="_ => el = _" */
+	getRef?: (el: HTMLElement | null) => void
 }>(), {
+	theme: undefined,
 	testWrapperMode: false,
 	outline: true,
 	forceOutline: false,
 	id: "app",
+	getRef: undefined,
 })
 
 const el = ref<HTMLElement | null>(null)
@@ -72,23 +76,24 @@ const el = ref<HTMLElement | null>(null)
 function handleRef(_: Element | ComponentPublicInstance | null): void {
 	if (_ !== null && !(_ instanceof HTMLElement)) unreachable()
 	el.value = _
-	props.ref?.(_)
+	props.getRef?.(_)
 }
 
 const autoOutline = useAccesibilityOutline(el).outline
 
 const showOutline = computed(() => (props.outline && autoOutline.value) || props.forceOutline)
 
+const theme = computed(() => props.theme ?? defaultTheme)
 const themeCb = (): void => {
-	theme.attach(el.value!)
+	toRaw(theme.value).attach(el.value!)
 }
-if ((process as any).client) {
+if (import.meta.client) {
 	onMounted(() => {
-		theme.on("change", themeCb)
+		toRaw(theme.value).on("change", themeCb)
 		themeCb()
 	})
 	onBeforeUnmount(() => {
-		theme.off("change", themeCb)
+		toRaw(theme.value).off("change", themeCb)
 	})
 }
 
