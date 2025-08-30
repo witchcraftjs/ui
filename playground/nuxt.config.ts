be496eb69b6b7c367ebe6ed64fed082970aad0cc
Alan North <alanscodelog@gmail.com>
2025-08-30 18:49:07 -0300

    deps: updated deps and minor improvements

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -5,8 +5,9 @@ export default defineNuxtConfig({
 		compatibilityVersion: 4 as const
 	},
 	modules: [
-		// "@witchcraft/ui/nuxt"
 		"../src/module"
+		// the below also works, just remember to run the update-dep script and uncomment ../src/module above before attempting to use the file: linked module
+		// "@witchcraft/ui/nuxt"
 	],
 	witchcraftUi: {
 		// only needed for the package's playground
